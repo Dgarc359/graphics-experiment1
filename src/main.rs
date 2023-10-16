e1ff505ad084e59cab294bdb41a764ae1f26090b
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2023-10-16 17:17:04 -0400

    creating beautiful lines

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,53 @@
+#[allow(dead_code, unused_variables, unused_braces)]
 extern crate sdl2;
 
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::{Color, PixelFormatEnum};
+use sdl2::pixels::{Color,PixelFormatEnum};
 use sdl2::event::Event;
 use std::time::Duration;
-use sdl2::rect::Rect;
+use sdl2::rect::{Rect, Point};
+const WIDTH: usize = 256;
+const HEIGHT: usize = 240;
+const PITCH: usize = std::mem::size_of::<u32>() * WIDTH;
 
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+
+    let window = video_subsystem.window("rust-sdl2 demo", 720, 480)
         .position_centered()
         .build()
         .unwrap();
 
+
     let mut canvas = window.into_canvas().build().unwrap();
-    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
 
     let texture_creator = canvas.texture_creator();
 
-    let mut texture = texture_creator
-        .create_texture_streaming(PixelFormatEnum::RGB24, 256, 256)
-        .map_err(|e| e.to_string()).unwrap();
-
-    texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
-        for y in 0..256 {
-            for x in 0..256 {
-                let offset = y * pitch + x * 3;
-                buffer[offset] = 0;
-                buffer[offset + 1] = 0;
-                buffer[offset + 2] = 0;
-            }
-        }
-    }).unwrap();
-
-    println!("updated texture");
-
-    canvas.clear();
-    canvas.copy(&texture, None, Some(Rect::new(100, 100, 256, 256))).unwrap();
-    canvas.copy_ex(
-        &texture,
-        None,
-        Some(Rect::new(450, 100, 256, 256)),
-        30.0,
-        None,
-        false,
-        false,
+    // create textures
+    let mut test = texture_creator.create_texture_streaming(
+        PixelFormatEnum::RGBA8888,
+        WIDTH as u32,
+        HEIGHT as u32,
     ).unwrap();
-    canvas.present();
 
+
+    canvas.present();
     let mut event_pump = sdl_context.event_pump().unwrap();
     'running: loop {
-        canvas.clear();
+
+        //const arr_width: usize = usize::try_from(WIDTH).unwrap() * 4;
+
+        let pixels_as_u8: &[u8] = &[255; 1024];
+        test.update(None, pixels_as_u8, PITCH).expect("couldnt copy raw pixels");
+
+        canvas.copy(&test, None, None).expect("couldnt copy texture to canvas");
+
+        canvas.present();
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. } |
@@ -64,7 +58,6 @@ fn main() {
             }
         }
 
-        canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 
