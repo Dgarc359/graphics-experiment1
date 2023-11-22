d9ffd7474fb186a243d8a10866866f91751e402b
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2023-11-22 18:12:18 -0500

    finish implementing trait

--- src/game.rs
+++ src/game.rs
@@ -9,13 +9,15 @@ pub mod game {
         fn get_width(&self) -> usize;
         fn get_height(&self) -> usize;
         /// Toggle the presence of a flag on a tile.
-        fn toggle_flag(&self, x: usize, y: usize);
+        fn toggle_flag(&mut self, x: usize, y: usize);
         /// Explore a tile. If it was a mine, BOOM! If not, flood fill mine scouting out from it.
         ///
         /// Returns true if BOOM!
-        fn explore_tile(&self, x: usize, y: usize) -> bool;
+        fn explore_tile(&mut self, x: usize, y: usize) -> bool;
         /// Return whether a tile should be displayed as blank, a flag, a cleared mine, a mine count 1-8, or a kaboom.
         fn get_tile(&self, x: usize, y: usize) -> Option<&MapTile>;
+
+        fn set_mine(&mut self, x: usize, y: usize) -> Option<MapTile>;
     }
 
     pub struct MapTile {
@@ -72,7 +74,7 @@ pub mod game {
         }
 
         /// Toggle the presence of a flag on a tile.
-        fn toggle_flag(&self, x: usize, y: usize) {
+        fn toggle_flag(&mut self, x: usize, y: usize) {
             let key = format!("{}:{}", x, y);
             let old_tile = self.game_state.map.get(&key).unwrap();
 
@@ -83,18 +85,32 @@ pub mod game {
                 has_mine: old_tile.has_mine,
             };
 
-            //self.to_owned().game_state.map.insert(key, new_tile);
+            self.game_state.map.insert(key, new_tile);
         }
         /// Explore a tile. If it was a mine, BOOM! If not, flood fill mine scouting out from it.
         ///
         /// Returns true if BOOM!
-        fn explore_tile(&self, _x: usize, _y: usize) -> bool {
-            true
+        fn explore_tile(&mut self, x: usize,y: usize) -> bool {
+            let key = format!("{}:{}", x, y);
+            return self.game_state.map.get(&key).unwrap().has_mine;
         }
         /// Return whether a tile should be displayed as blank, a flag, a cleared mine, a mine count 1-8, or a kaboom.
         fn get_tile(&self, x: usize, y: usize) -> Option<&MapTile> {
             let key = &format!("{}:{}",x, y);
             self.game_state.map.get(key)
+        }
+
+        fn set_mine(&mut self, x: usize, y: usize) -> Option<MapTile> {
+
+            let key = format!("{}:{}", x, y);
+            let old_tile = self.game_state.map.get(&key).unwrap();
+            let new_tile = MapTile {
+                x: old_tile.x,
+                y: old_tile.y,
+                has_flag: old_tile.has_flag,
+                has_mine: !old_tile.has_mine,
+            };
+            self.game_state.map.insert(key, new_tile)
         }
     }
 }
@@ -113,6 +129,28 @@ mod tests {
     }
 
     #[test]
+    fn test_toggle_flag() {
+       let mut new_game = game::new_game(10, 10, 3);
+
+        let res = new_game.get_tile(1, 1).unwrap().to_owned();
+        assert_eq!(false, res.has_flag);
+        new_game.toggle_flag(1,1);
+        let res = new_game.get_tile(1, 1).unwrap().to_owned();
+        assert_eq!(true, res.has_flag);
+    }
+
+    #[test]
+    fn test_has_mine() {
+        let mut game = game::new_game(10, 10, 3);
+        let res = game.get_tile(1,1).unwrap().to_owned();
+        assert_eq!(false, res.has_mine);
+        game.set_mine(1,1);
+
+        let res = game.get_tile(1,1).unwrap().to_owned();
+        assert_eq!(true, res.has_mine);
+    }
+
+    #[test]
     fn test_map() {
         let new_game = game::new_game(10, 10, 3);
 
@@ -120,6 +158,5 @@ mod tests {
         // TODO: refactor test or remove or adjust, because mines will be added randomly
         assert_eq!(false, res.has_mine);
         assert_eq!(false, res.has_flag);
-
     }
 }
